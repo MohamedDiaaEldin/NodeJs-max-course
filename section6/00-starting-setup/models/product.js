dfebcca9ee2f681b3852606de503fa76cd6ae7c6
MohamedDiaaEldin <mdiaan404@gmail.com>
2023-12-06 02:46:18 +0200

    start with section 7

--- section6/00-starting-setup/models/product.js
+++ section6/00-starting-setup/models/product.js
@@ -1,6 +1,10 @@
-const path = require('path')
-const fs = require('fs')
-const filePath = path.join(path.dirname(require.main.filename), 'data', 'products.json');
+const path = require("path");
+const fs = require("fs");
+const filePath = path.join(
+  path.dirname(require.main.filename),
+  "data",
+  "products.json"
+);
 
 module.exports = class Product {
   constructor(title) {
@@ -8,41 +12,40 @@ module.exports = class Product {
   }
 
   // This function is responsible for saving the current object (this) to a JSON file.
-save() {
-  // Constructing the file path where the data will be stored
+  save() {
+    // Constructing the file path where the data will be stored
 
-  // Read the existing products data from the products.json file
-  fs.readFile(filePath, (error, fileContent) => {
+    // Read the existing products data from the products.json file
+    fs.readFile(filePath, (error, fileContent) => {
       // Create an empty array to store products
       let products = [];
 
       // Check if there was no error reading the file
       if (!error) {
-          // Parse the existing file content into an array of products
-          products = JSON.parse(fileContent);
+        // Parse the existing file content into an array of products
+        products = JSON.parse(fileContent);
       }
 
       // Add the current object (this) to the products array
       products.push(this);
 
       // Write the updated products array back to the products.json file
-      fs.writeFile(filePath, JSON.stringify(products), (error)=> {
-          // Check if there was an error writing to the file
-          if (error) {
-              console.log(error)
-          }
+      fs.writeFile(filePath, JSON.stringify(products), (error) => {
+        // Check if there was an error writing to the file
+        if (error) {
+          console.log(error);
+        }
       });
-  });
-}
+    });
+  }
 
   static fetchAll(cb) {
-    fs.readFile(filePath, (error, fileContent)=>{
-
-      if(error || fileContent === undefined ){
-         return cb([])
+    fs.readFile(filePath, (error, fileContent) => {
+      if (error || fileContent === undefined) {
+        return cb([]);
       }
-      
-       cb(JSON.parse(fileContent))
-    })
+
+      cb(JSON.parse(fileContent));
+    });
   }
 };
